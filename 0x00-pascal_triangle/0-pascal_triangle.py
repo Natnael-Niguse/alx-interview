6d74172be28a38079d5f80199be64f37694fbc99
Natnael-Niguse <natiart07@gmail.com>
2024-01-06 16:24:50 +0000

    python3

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python3
 """
-0-pascal_triangle
+pascal_triangle
 """
 
+
 def pascal_triangle(n):
+    """
+    Returns a list of integers
+    representing the Pascal Triangle of n
+    returns empty list if n <= 0
+    """
     k = []
     if n <= 0:
         return k
     k = [[1]]
-
     for i in range(1, n):
         temp = [1]
         for j in range(len(k[i - 1]) - 1):
